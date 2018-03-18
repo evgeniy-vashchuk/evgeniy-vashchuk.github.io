6d275d2c8e4a03fac98d043439f3d01862528904
Evgeniy Vashchuk <evgeniy.vashchuk.mail@gmail.com>
2018-03-18 23:27:13 +0200

    add x-project-components

---
+++ x-project-components/js/common.js
@@ -0,0 +1,48 @@
+$( document ).ready(function() {
+	// show code button arrow
+	$('.collapse').on('show.bs.collapse', function () {
+		$(this).parent('.component-block-usage').find('.btn').addClass('-open');
+	})
+
+	$('.collapse').on('hide.bs.collapse', function () {
+		$(this).parent('.component-block-usage').find('.btn').removeClass('-open');
+	})
+
+	// scroll line
+	window.onscroll = function() {
+		myFunction()
+	};
+
+	function myFunction() {
+		var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+		var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+		var scrolled = (winScroll / height) * 100;
+		document.getElementById("myBar").style.width = scrolled + "%";
+	}
+
+	// search
+	$('.js-search-input').hideseek({
+		highlight: true,
+		nodata: 'No results found'
+	});
+
+	// scroll to element after search
+	$('.js-search-input').on("_after", function() {
+		$('.js-list-group > .list-group-item').each(function() {
+			if ($(this).is(':visible')) {
+
+				$("html, body").animate({
+					scrollTop: $($(this).attr("href")).offset().top
+				}, 0);
+
+				return false;
+			}
+		});
+	});
+
+});
+
+// replase '<' and '>' in html code examples
+document.querySelectorAll(".js-replase-brackets").forEach(function(element) {
+	element.innerHTML = element.innerHTML.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+});
