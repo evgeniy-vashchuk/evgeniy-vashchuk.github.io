1c9f96a6caec1ed721c87411e05b0a98932868dc
Evgeniy Vashchuk <evgeniy.vashchuk.mail@gmail.com>
2018-05-06 23:11:09 +0300

    refactoring + some small changes

--- x-project-components/js/common.js
+++ x-project-components/js/common.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	// show code button arrow
+	// SHOW CODE BUTTON ARROW
 	$('.collapse').on('show.bs.collapse', function () {
 		$(this).parent('.component-block-usage').find('.btn').addClass('-open');
 	})
@@ -8,30 +8,30 @@ $(document).ready(function() {
 		$(this).parent('.component-block-usage').find('.btn').removeClass('-open');
 	})
 
-	// scroll line
-	window.onscroll = function() {
-		myFunction()
-	};
-
-	function myFunction() {
-		var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-		var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-		var scrolled = (winScroll / height) * 100;
-		document.getElementById("myBar").style.width = scrolled + "%";
+	// PROGRESS BAR
+	function progressBar() {
+		var winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+				height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+				scrolled = (winScroll / height) * 100;
+		document.getElementById('progress-bar').style.width = scrolled + "%";
 	}
 
-	// search
+	window.onscroll = function() {
+		progressBar()
+	};
+
+	// SEARCH
 	$('.js-search-input').hideseek({
 		highlight: true,
 		nodata: 'No results found'
 	});
 
 	// scroll to element after search
-	$('.js-search-input').on("_after", function() {
+	$('.js-search-input').on('_after', function() {
 		$('.js-list-group > .list-group-item').each(function() {
 			if ($(this).is(':visible')) {
 
-				$("html, body").animate({
+				$('html, body').animate({
 					scrollTop: $($(this).attr("href")).offset().top
 				}, 0);
 
@@ -40,9 +40,9 @@ $(document).ready(function() {
 		});
 	});
 
-	// scroll top button hide on < 1200
+	// SCROLL TOP BUTTON HIDE ON < 1500
 	function scrollTopButtonHide() {
-		if ($(window).width() < 1200) {
+		if ($(window).width() < 1500) {
 
 			$(window).on('scroll', function() {
 				if ($(this).scrollTop() > 100) {
@@ -61,21 +61,33 @@ $(document).ready(function() {
 
 	scrollTopButtonHide();
 
-	$(window).resize(function() {
+	$(window).on('resize', function() {
 		scrollTopButtonHide();
 	});
 
-	// scroll top button with footer
-	$(window).on('scroll', function() {
-		var pageHeight = $(document).height(),
-				bottomOfPage = $(document).scrollTop() + $(window).height(),
-				footerHeight = $('.js-footer').outerHeight();
+	// SCROLL TOP BUTTON WITH FOOTER
+	function scrollTopBtnWithFooter() {
+		$(window).on('scroll', function() {
+			if ($(window).width() < 1500) {
+					var pageHeight = $(document).height(),
+							bottomOfPage = $(document).scrollTop() + $(window).height(),
+							footerHeight = $('.js-footer').outerHeight();
 
-		if (pageHeight === bottomOfPage) {
-			$('.js-scroll-top-button').css('bottom', footerHeight);
-		} else {
-			$('.js-scroll-top-button').css('bottom', 0);
-		}
+					if (pageHeight === bottomOfPage) {
+						$('.js-scroll-top-button').css('bottom', footerHeight);
+					} else {
+						$('.js-scroll-top-button').css('bottom', 0);
+					}
+			} else {
+				$('.js-scroll-top-button').css('bottom', 0);
+			}
+		});
+	}
+
+	scrollTopBtnWithFooter();
+
+	$(window).on('resize', function() {
+		scrollTopBtnWithFooter()
 	});
 
 });
