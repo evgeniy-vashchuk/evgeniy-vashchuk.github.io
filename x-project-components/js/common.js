a1e74c3d96881bbb3ea606871d9eaa56b8bc3783
Evgeniy Vashchuk <evgeniy.vashchuk.mail@gmail.com>
2018-05-20 17:50:03 +0300

    Added count up component, added function for scroll bar compensation, fixed decimal converting in progress bars

--- x-project-components/js/common.js
+++ x-project-components/js/common.js
@@ -90,4 +90,113 @@ $(document).ready(function() {
 		scrollTopBtnWithFooter()
 	});
 
+	// SIDE MENU ON MOBILE
+	var slideout = new Slideout({
+		'panel': document.getElementById('main-wrap'),
+		'menu': document.getElementById('side-navigation-mobile'),
+		'padding': 256,
+		'tolerance': 70,
+		'side': 'right',
+		'touch': false
+	});
+
+	$('.js-hamburger-menu').on('click', function() {
+		$(this).toggleClass('-open-menu');
+		slideout.toggle();
+	});
+
+	// translate fixed element
+	var fixed = document.querySelectorAll('.js-header, .js-scroll-top-button');
+
+	slideout.on('translate', function(translated) {
+		for (var i = 0; i < fixed.length; i++) {
+			fixed[i].style.transform = 'translateX(' + translated + 'px)';;
+		}
+	});
+
+	slideout.on('beforeopen', function () {
+		for (var i = 0; i < fixed.length; i++) {
+			fixed[i].style.transition = 'transform 300ms ease';
+			fixed[i].style.transform = 'translateX(-256px)';
+		}
+	});
+
+	slideout.on('beforeclose', function () {
+		for (var i = 0; i < fixed.length; i++) {
+			fixed[i].style.transition = 'transform 300ms ease';
+			fixed[i].style.transform = 'translateX(0px)';
+		}
+	});
+
+	slideout.on('open', function () {
+		for (var i = 0; i < fixed.length; i++) {
+			fixed[0].style.transition = '';
+		}
+	});
+
+	slideout.on('close', function () {
+		for (var i = 0; i < fixed.length; i++) {
+			fixed[i].style.transition = '';
+		}
+	});
+
+	// overlay
+	function close(eve) {
+		eve.preventDefault();
+		slideout.close();
+	}
+
+	slideout
+		.on('beforeopen', function() {
+			this.panel.classList.add('-open');
+		})
+		.on('open', function() {
+			this.panel.addEventListener('click', close);
+		})
+		.on('beforeclose', function() {
+			this.panel.classList.remove('-open');
+			this.panel.removeEventListener('click', close);
+			$('.js-hamburger-menu').removeClass('-open-menu');
+		});
+
+	$('.js-list-group .list-group-item').on('click', function() {
+		slideout.close();
+	});
+
+	// SCROLLSPY
+	$('body').scrollspy({target: '.js-scroll-spy'});
+
+
+	// TAB-SIZE POLYFILL
+	var tabSize = 2;
+	var codeElements = document.getElementsByTagName('code'),
+			pre = document.getElementsByTagName('pre'),
+			e = document.createElement('i');
+
+	if(e.style.tabSize !== '' && e.style.MozTabSize !== '' && e.style.oTabSize !== '')
+	{
+		replaceTabs(codeElements);
+		replaceTabs(pre);
+	}
+
+	function replaceTabs(ele)
+	{
+		for(var i = 0; i < ele.length; i++)
+		{
+			ele[i].innerHTML = ele[i].innerHTML.replace(/\t/g, repeat(" ", tabSize));
+		}
+	}
+
+	function repeat(st, n) {
+		var s = "";
+		while (--n >= 0) {
+			s += st
+		}
+		return s;
+	}
+
+	// POSITION STICKY POLIFILL
+	var elements = $('.js-side-navigation');
+	Stickyfill.add(elements);
+
 });
